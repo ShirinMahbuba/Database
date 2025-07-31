c72d087a71f09e840f08ce577ababd0882081527
Shirin Mahbuba Sara <mahbubasara3@gmail.com>
2025-08-01 00:18:36 +0600

    Add files via upload

---
+++ Handling NULL Values and Defaults.sql
@@ -0,0 +1,22 @@
+--INSERT INTO Students (StudentID,Name)
+--VALUES (7,NULL);
+/*
+USE Studentdb;
+SELECT * FROM Students
+WHERE Name is NULL; */
+
+/*
+CREATE TABLE Course (
+CourseID INT PRIMARY KEY,
+Title NVARCHAR(100),
+CreateDate DATE DEFAULT GETDATE()
+);
+
+*/
+/*
+
+USE Studentdb;;
+INSERT INTO Course (CourseID,Title)
+VALUES (1,'Intro SQL');*/
+
+SELECT * FROM Course;
